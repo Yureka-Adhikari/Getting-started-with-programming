2c69ff97b2aa68698bef04b9dc67e2b0a70fe9e6
Yureka Adhikari <adhikariyureka@gmail.com>
2024-11-07 19:16:06 +0545

    first commit

---
+++ Welcome.py
@@ -0,0 +1,3 @@
+print("Hello World,Welcome to the world of programming!")
+print(45+50)
+print("My name is Yureka,\nmy age is:", 13)
---
+++ variables.py
@@ -0,0 +1,8 @@
+num1 = 15
+num2 = 25
+sum = num1 + num2
+
+name = "Yureka"
+
+print("My name is", name, "\nMy age is ", 13, "\nSum of num1 + num2 = ", num1+num2)
+print(f"My name is {name} \nMy age is {13} \nSum of {num1} and {num2} is {sum}")
